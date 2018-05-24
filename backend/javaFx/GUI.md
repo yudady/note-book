814ec2eba5fea7b855899464c00c2cdf6c9255ea
yu_dady@yahoo.com.tw <yu650715>
2018-05-24 16:08:58 +0800

    fx

---
+++ backend/javaFx/GUI.md
@@ -0,0 +1,21 @@
+# java gui
+
+
+## JavaFX 8
+
+[JavaFX 8 教程（中文）](http://code.makery.ch/library/javafx-8-tutorial/zh-cn/)
+
+* 第一部分：Scene Builder
+* 第二部分：Model 和 TableView
+* 第三部分：与用户的交互
+* 第四部分：CSS 样式
+* 第五部分：将数据用 XML 格式存储
+* 第六部分：统计图
+* 第七部分：部署
+
+## Java Robot
+
+[Java robot key press, mouse move and click](https://blog.softhints.com/java-robot-key-press-mouse-move-and-click/)
+
+[GUI automation using a Robot](https://undocumentedmatlab.com/blog/gui-automation-robot)
+
--- backend/javaFx/javafxDemo/src/main/java/com/tommy/fx05/MainApp.java
+++ backend/javaFx/javafxDemo/src/main/java/com/tommy/fx05/MainApp.java
@@ -1,8 +1,10 @@
-package com.tommy.fx04;
+package com.tommy.fx05;
 
-import com.tommy.fx04.model.Person;
-import com.tommy.fx04.view.PersonEditDialogController;
-import com.tommy.fx04.view.PersonOverviewController;
+import com.tommy.fx05.model.Person;
+import com.tommy.fx05.model.PersonListWrapper;
+import com.tommy.fx05.view.PersonEditDialogController;
+import com.tommy.fx05.view.PersonOverviewController;
+import com.tommy.fx05.view.RootLayoutController;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -13,8 +15,15 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import org.controlsfx.dialog.Dialogs;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
 import java.io.IOException;
+import java.util.prefs.Preferences;
+
 public class MainApp extends Application {
 
 	private Stage primaryStage;
@@ -29,7 +38,6 @@ public class MainApp extends Application {
 	 * Constructor
 	 */
 	public MainApp() {
-
 		// Add some sample data
 		personData.add(new Person("Hans", "Muster"));
 		personData.add(new Person("Ruth", "Mueller"));
@@ -42,11 +50,15 @@ public class MainApp extends Application {
 		personData.add(new Person("Martin", "Mueller"));
 	}
 
-	@Override public void start(Stage primaryStage) throws Exception {
+	@Override
+	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
 		this.primaryStage.setTitle("AddressApp");
 
+		// Set the application icon.
+
 		String pngResource = MainApp.class.getResource("/image/address_book_32.png").toString();
+
 		this.primaryStage.getIcons().add(new Image(pngResource));
 
 		initRootLayout();
@@ -55,7 +67,7 @@ public class MainApp extends Application {
 	}
 
 	/**
-	 * Initializes the root layout.
+	 * Initializes the root layout and tries to load the last opened person file.
 	 */
 	public void initRootLayout() {
 		try {
@@ -67,9 +79,20 @@ public class MainApp extends Application {
 			// Show the scene containing the root layout.
 			Scene scene = new Scene(rootLayout);
 			primaryStage.setScene(scene);
+
+			// Give the controller access to the main app.
+			RootLayoutController controller = loader.getController();
+			controller.setMainApp(this);
+
 			primaryStage.show();
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+
+		// Try to load last opened person file.
+		File file = getPersonFilePath();
+		if (file != null) {
+			loadPersonDataFromFile(file);
 		}
 	}
 
@@ -96,11 +119,12 @@ public class MainApp extends Application {
 	}
 
 	/**
-	 * Opens a dialog to edit details for the specified person. If the user
-	 * clicks OK, the changes are saved into the provided person object and true
-	 * is returned.
+	 * Opens a dialog to edit details for the specified person. If the user clicks
+	 * OK, the changes are saved into the provided person object and true is
+	 * returned.
 	 *
-	 * @param person the person object to be edited
+	 * @param person
+	 *            the person object to be edited
 	 * @return true if the user clicked OK, false otherwise.
 	 */
 	public boolean showPersonEditDialog(Person person) {
@@ -117,6 +141,7 @@ public class MainApp extends Application {
 			dialogStage.initOwner(primaryStage);
 
 			String pngResource = MainApp.class.getResource("/image/edit.png").toString();
+
 			dialogStage.getIcons().add(new Image(pngResource));
 			Scene scene = new Scene(page);
 			dialogStage.setScene(scene);
@@ -133,6 +158,97 @@ public class MainApp extends Application {
 		} catch (IOException e) {
 			e.printStackTrace();
 			return false;
+		}
+	}
+
+	/**
+	 * Returns the person file preference, i.e. the file that was last opened. The
+	 * preference is read from the OS specific registry. If no such preference can
+	 * be found, null is returned.
+	 *
+	 * @return
+	 */
+	public File getPersonFilePath() {
+		Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+		String filePath = prefs.get("filePath", null);
+		if (filePath != null) {
+			return new File(filePath);
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * Sets the file path of the currently loaded file. The path is persisted in the
+	 * OS specific registry.
+	 *
+	 * @param file
+	 *            the file or null to remove the path
+	 */
+	public void setPersonFilePath(File file) {
+		Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+		if (file != null) {
+			prefs.put("filePath", file.getPath());
+
+			// Update the stage title.
+			primaryStage.setTitle("AddressApp - " + file.getName());
+		} else {
+			prefs.remove("filePath");
+
+			// Update the stage title.
+			primaryStage.setTitle("AddressApp");
+		}
+	}
+
+	/**
+	 * Loads person data from the specified file. The current person data will be
+	 * replaced.
+	 *
+	 * @param file
+	 */
+	public void loadPersonDataFromFile(File file) {
+		try {
+			JAXBContext context = JAXBContext.newInstance(PersonListWrapper.class);
+			Unmarshaller um = context.createUnmarshaller();
+
+			// Reading XML from the file and unmarshalling.
+			PersonListWrapper wrapper = (PersonListWrapper) um.unmarshal(file);
+
+			personData.clear();
+			personData.addAll(wrapper.getPersons());
+
+			// Save the file path to the registry.
+			setPersonFilePath(file);
+
+		} catch (Exception e) { // catches ANY exception
+			Dialogs.create().title("Error").masthead("Could not load data from file:\n" + file.getPath())
+					.showException(e);
+		}
+	}
+
+	/**
+	 * Saves the current person data to the specified file.
+	 *
+	 * @param file
+	 */
+	public void savePersonDataToFile(File file) {
+		try {
+			JAXBContext context = JAXBContext.newInstance(PersonListWrapper.class);
+			Marshaller m = context.createMarshaller();
+			m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+			// Wrapping our person data.
+			PersonListWrapper wrapper = new PersonListWrapper();
+			wrapper.setPersons(personData);
+
+			// Marshalling and saving XML to the file.
+			m.marshal(wrapper, file);
+
+			// Save the file path to the registry.
+			setPersonFilePath(file);
+		} catch (Exception e) { // catches ANY exception
+			Dialogs.create().title("Error").masthead("Could not save data to file:\n" + file.getPath())
+					.showException(e);
 		}
 	}
 
